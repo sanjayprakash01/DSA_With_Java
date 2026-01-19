907ea34e858b81643db2aa5a710f9f3d7994950f
Sanjayprakash <msanjayprakash@outlook.com>
2026-01-19 21:22:41 +0530

    add SearchInRotatedSortArray 33

---
+++ SearchInRotatedSortArray_33.java
@@ -0,0 +1,20 @@
+public class SearchInRotatedSortArray_33 {
+    int search(int[] nums, int target) {
+        if (nums.length == 0) return 0;
+        int start = 0;
+        if (nums[start] >= target) {//4,5,6,7,0,1,2
+            start = nums.length - 1;
+            System.out.println("++++++++++++++++"+ start);
+            while (start >= 0) {
+                if (nums[start] == target) return start; 
+                start--;
+            }
+        } else if (nums[start] < target) {
+            while (start <= nums.length - 1) {
+                if (nums[start] == target) return start;
+                start++;
+            }
+        }
+        return -1;
+    }
+}
